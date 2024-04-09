50bc343c9c9e58be5532c2c452b92a1f54fe214d
Pima <chandan.suthar@pima.in>
2024-04-09 14:45:08 +0530

    Working on HomePage Tags

--- ProjectFiles/NetSolution/ClockLogic.cs
+++ ProjectFiles/NetSolution/ClockLogic.cs
@@ -25,7 +25,27 @@ public class ClockLogic : BaseNetLogic {
     private void UpdateTime() {
         LogicObject.GetVariable("Time").Value = DateTime.Now;
         LogicObject.GetVariable("UTCTime").Value = DateTime.UtcNow;
+
+        DateTime currentTime = DateTime.Now;
+        string currentDate = DateTime.Now.ToString("yyyy-MM-dd");
+        int currentHour = currentTime.Hour;
+
+        // Calculate start and end times for the current day
+        DateTime startTime = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, 8, 0, 0);
+        DateTime endTime = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, 7, 59, 59).AddDays(1);
+        DateTime todaydate = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, 0, 0, 0);
+        var date1 = startTime.ToString("dd-MM-yyyy");
+        // Adjust the start time if the current hour is before 8 AM
+        if (currentHour < 8)
+        {
+            startTime = startTime.AddDays(-1);
+            endTime = endTime.AddDays(-1);
+        }
+        LogicObject.GetVariable("TodayStartTime").Value = startTime;
+        LogicObject.GetVariable("TodayEndTime").Value = endTime;
+        LogicObject.GetVariable("TodayDate").Value = todaydate;
     }
 
     private PeriodicTask periodicTask;
 }
+
